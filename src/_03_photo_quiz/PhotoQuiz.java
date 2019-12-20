3212667afcfd1042efbc14c8411705c4460ecbc6
League Student <league@iMac-14.attlocal.net>
2019-12-19 18:27:00 -0800

    :knife: :cry:
    :

--- src/_03_photo_quiz/PhotoQuiz.java
+++ src/_03_photo_quiz/PhotoQuiz.java
@@ -26,35 +26,49 @@ public class PhotoQuiz {
 		// 1. find an image on the internet, and put its URL in a String
 		// variable (from your browser, right click on the image, and select
 		// “Copy Image Address”)
-
+		String imgURL = "https://i.ytimg.com/vi/RCSoABZbtOU/maxresdefault.jpg";
 		// 2. create a variable of type "Component" that will hold your image
-
+		Component component;
 		// 3. use the "createImage()" method below to initialize your Component
-
+		component = createImage(imgURL);
 		// 4. add the image to the quiz window
-
+		quizWindow.add(component);
 		// 5. call the pack() method on the quiz window
-
+		quizWindow.pack();
 		// 6. ask a question that relates to the image
-
+		String answer = JOptionPane.showInputDialog("Does this look like you?");
 		// 7. print "CORRECT" if the user gave the right answer
-
+		if (answer.equals("No")) {
+			JOptionPane.showMessageDialog(null, "Incorrect");
+		} else if (answer.equals("Yes")) {
+			JOptionPane.showMessageDialog(null, "Correct");
+		}
 		// 8. print "INCORRECT" if the answer is wrong
 
 		// 9. remove the component from the quiz window (you may not see the
 		// effect of this until step 12)
-
+		quizWindow.remove(component);
+		
 		// 10. find another image and create it (might take more than one line
 		// of code)
+		String imageno2 = "https://i.ytimg.com/vi/WNego9ETfo0/maxresdefault.jpg";
+		Component componentno2;
+		componentno2 = createImage(imageno2);
 
 		// 11. add the second image to the quiz window
+		quizWindow.add(componentno2);
 
 		// 12. pack the quiz window
+		quizWindow.pack();
 
 		// 13. ask another question
-
+		String answerno2 = JOptionPane.showInputDialog("Is this you");
 		// 14+ check answer, say if correct or incorrect, etc.
-
+		if (answerno2.equals("No")) {
+			JOptionPane.showMessageDialog(null, "Incorrect");
+		} else if (answerno2.equals("Yes")) {
+			JOptionPane.showMessageDialog(null, "Correct");
+		}
 	}
 
 	private Component createImage(String imageUrl) throws MalformedURLException {
--- src/_04_drum_kit/DrumKit.java
+++ src/_04_drum_kit/DrumKit.java
@@ -25,35 +25,36 @@ public class DrumKit implements MouseListener {
 	public void run() throws MalformedURLException {
 
 		// 1. Make a JFrame variable and initialize it using "new JFrame()"
-
+		JFrame frame = new JFrame();
 		// 2. Make the frame visible and
 		// set its default close operation to JFrame.EXIT_ON_CLOSE
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		// 3. Set the size of the frame
-
+		frame.setSize(5000, 1000);
 		// 4. Set the title of the frame
-
+		frame.setTitle("Drum Set");
 		// 5. Make a JPanel variable and initialize it using "new JPanel().
-
+		JPanel panel = new JPanel();
 		// 6. Add the panel to the frame. (The panel is invisible.)
-
+		frame.add(panel);
 		// 7. Download an image of a drum from the Internet. Drop it into your
 		// Eclipse project under "default package".
 
 		// 8. Put the name of your image file in a String variable.
-
+		String imageName = "snare.jpg";
 		// 9. Edit the next line to use your String variable
 		// drumLabelWithImage = createLabelImage(drumImageString);
-
+		drumLabelWithImage = createLabelImage(imageName);
 		// 10. Add the image to the panel
-
+		panel.add(drumLabelWithImage);
 		// 11. Set the layout of the panel to "new GridLayout()"
-
+		panel.setLayout(new GridLayout());
 		// 12. call the pack() method on the frame. Run your program. Do you see
 		// your drum image?
-
+		frame.pack();
 		// 13. add this mouse listener to drumLabelWithImage
-
+		drumLabelWithImage.addMouseListener(this);
 		// 18. Add more images to make a drumkit. Remember to add this mouse
 		// listener to each one.
 
